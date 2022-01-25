0695536c39336d48338c8394b1f4b23bad7c928b
AlexBalew <balew48@gmail.com>
2022-01-25 19:14:27 +0300

    fonts loader, csv, xml, jquery, extensions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import Post from './post';
+import * as $ from 'jquery'
+import Post from '@models/post'; //alias webpack config. Создаем абсолютный путь
 import './styles/styles.css'
 import json from './assets/json.json'
-import WebpackLogo from './assets/webpack-logo.png'
+import WebpackLogo from './assets/webpack-logo' //расширение файла не пишем, т.к. они указаны в extensions webpack config
+import xml from '@/assets/data.xml' //@ заменяет . или  src. alias webpack config
+import csv from './assets/data.csv'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
 
+$('pre').html(post.toString())
+
 console.log('Post to String:', post.toString())
 console.log('JSON:', json)
+console.log('XML:', xml)
+console.log('CSV:', csv)
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
         filename: '[name].[contenthash].js', //naming pattern, unique bundle names
         path: path.resolve(__dirname, 'dist'),
     },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@models': path.resolve(__dirname, 'src/models'),
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html',
@@ -28,6 +35,18 @@ module.exports = {
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader']
             }
         ]
     }
